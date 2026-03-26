a30ded119a26430e7ac985bdc3a17059a0158f87
dhymasws <dimaswahyuara@gmail.com>
2026-03-26 11:01:29 +0700

    initial commit

---
+++ src/main/java/com/test/msignotificationservice/MsigNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.test.msignotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsigNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsigNotificationServiceApplication.class, args);
+    }
+
+}
